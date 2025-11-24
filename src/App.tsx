6734ff82a62018ce73067c4387f1d883c9f8f9ce
CNieblesR <niebles.reyes@gmail.com>
2025-11-24 18:48:27 -0500

    feat: add table component and Tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'wouter'
 import './App.css'
 import Home from './Home'
 import Terms from './Terms'
+import Admin from './Admin'
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
     <Switch>
       <Route path="/" component={Home} />
       <Route path="/terms" component={Terms} />
+      <Route path="/admin" component={Admin} />
     </Switch>
   )
 }
